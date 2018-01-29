d13ad2f5d346e70b6f9167a29a3278c0f9a0bcf0
Kevin.O <ckoae@connect.ust.hk>
2018-01-30 02:00:10 +0800

    update login page, 2 demo ac for testing
    test@test.com
    test@test.com

--- app/src/main/java/lix5/ushare/LoginActivity.java
+++ app/src/main/java/lix5/ushare/LoginActivity.java
@@ -3,40 +3,90 @@ package lix5.ushare;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
     Button b1,b2;
     EditText ed1,ed2;
 
-    TextView tx1;
-    int counter = 3;
+    private FirebaseAuth mAuth; //instance of FirebaseAuth
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // updateUI(currentUser);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
-        b1 = (Button)findViewById(R.id.button);
         ed1 = (EditText)findViewById(R.id.editText);
         ed2 = (EditText)findViewById(R.id.editText2);
 
+        b1 = (Button)findViewById(R.id.button);
         b2 = (Button)findViewById(R.id.button2);
-        tx1 = (TextView)findViewById(R.id.textView3);
 
         b1.setOnClickListener(new View.OnClickListener() {
            public void onClick(View v){
-               Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-               LoginActivity.this.startActivity(intent);
+               signIn(ed1.getText().toString(), ed2.getText().toString());
            }
         });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+
+    private void signIn(String email, String password){
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Login successful",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            LoginActivity.this.startActivity(intent);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            //exception
+                        }
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
     }
 }
