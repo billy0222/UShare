e87d106f5e45b674ab18de2cdc9086288a176e42
Kevin.O <ckoae@connect.ust.hk>
2018-02-05 21:21:59 +0800

    fix

--- app/src/main/java/lix5/ushare/LoginActivity.java
+++ app/src/main/java/lix5/ushare/LoginActivity.java
@@ -1,24 +1,15 @@
 package lix5.ushare;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
 
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
--- app/src/main/java/lix5/ushare/RegActivity.java
+++ app/src/main/java/lix5/ushare/RegActivity.java
@@ -2,7 +2,6 @@ package lix5.ushare;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.text.TextUtils;
@@ -11,9 +10,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
@@ -60,7 +56,7 @@ public class RegActivity extends AppCompatActivity {
         });
 
         mAuthListener = firebaseAuth -> {
-            FirebaseUser user = firebaseAuth.getCurrentUser();
+            FirebaseUser user = mAuth.getCurrentUser();
             if(user != null){
                 sendVerificationEmail();
             }
