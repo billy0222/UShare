7e38f953921e4ef2bfbfe92a3b3a0a526cc84243
Kevin.O <ckoae@connect.ust.hk>
2018-04-15 21:50:35 +0800

    Taxi fare cal

---
+++ app/src/main/java/lix5/ushare/MyFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package lix5.ushare;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage){
+        System.out.println("MESSAGE RECEIVED");
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
---
+++ app/src/main/java/lix5/ushare/PlaceDistance.java
@@ -0,0 +1,49 @@
+package lix5.ushare;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Kevin on 15/4/2018.
+ */
+
+public class PlaceDistance {
+    private int distance;
+    private int duration;
+
+    static PlaceDistance getDistanceJsonToPlace(JSONObject object) {
+        try {
+            PlaceDistance result = new PlaceDistance();
+            JSONArray elements = object.getJSONArray("elements");
+            JSONObject temp = elements.getJSONObject(0);
+            JSONObject distance = (JSONObject) temp.get("distance");
+            JSONObject duration = (JSONObject) temp.get("duration");
+            result.setDistance((int) distance.get("value"));
+            result.setDuration((int) duration.get("value"));
+            return result;
+        } catch (JSONException ex) {
+            Logger.getLogger(Place.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
